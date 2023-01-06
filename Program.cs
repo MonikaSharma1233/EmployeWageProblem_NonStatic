17e0cf147540ff9e4afdc751869c0720b0813f7b
firsttoken <monikasharma12330@gmail.com>
2023-01-06 09:31:26 +0530

    Adding the Present or absent

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using EmployeeWageProblem;
+namespace EmployeeWageProblem;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        //Constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absend");
+        }
+
+
+        Console.ReadKey();
+    }
+}
+
+
