eac8850c5ecc85c171243a947c6afde15f7dcb45
firsttoken <monikasharma12330@gmail.com>
2023-01-13 19:27:38 +0530

    Refector the code

---
+++ Program_UC7.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using EmployeeWageProblem;
+using System.Xml.Schema;
+
+namespace EmployeeWageProblem;
+class Program
+{
+    //Constants
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public const int NUM_OF_WORKING_DAYS = 20;
+    public const int MAX_HRS_IN_MONTH = 100;
+    public static int ComputeEmpWage()
+    {
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        //Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            totalEmpHrs += empHrs;
+            Console.WriteLine("Days  : " + totalWorkingDays + "Emp Hrs : " + totalEmpHrs);
+        }
+        totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        return totalEmpWage;
+    }
+    public static void Main(String[] args)
+    {
+
+        ComputeEmpWage();
+
+
+
+        Console.ReadKey();
+    }
+
+
+}
+
+
